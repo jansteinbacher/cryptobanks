3c6a42798ede4a0a2ab46570e2b582caf9680f11
jansteinbacher <116812558+jansteinbacher@users.noreply.github.com>
2023-05-01 20:14:37 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,26 +7,7 @@ module.exports = {
       display: ["Open Sans", "sans-serif"],
       body: ["Open Sans", "sans-serif"],
     },
-    extend: {
-      screens: {
-        mf: "990px",
-      },
-      keyframes: {
-        "slide-in": {
-          "0%": {
-            "-webkit-transform": "translateX(120%)",
-            transform: "translateX(120%)",
-          },
-          "100%": {
-            "-webkit-transform": "translateX(0%)",
-            transform: "translateX(0%)",
-          },
-        },
-      },
-      animation: {
-        "slide-in": "slide-in 0.5s ease-out",
-      },
-    },
+    
   },
   variants: {
     extend: {},
